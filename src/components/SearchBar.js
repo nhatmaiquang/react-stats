c9df72cc84d665b22eebce593131bddc167bc507
Mai Quang Nhat <quangnhat224@gmail.com>
2019-04-09 11:38:40 +0300

    styled search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,14 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>Search Bar</div>
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Tên người triệu hồi</label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
     );
   }
 }
