74d464c90a074a442ceb183c44ed3a82f192c6bb
Mai Quang Nhat <quangnhat224@gmail.com>
2019-04-11 10:43:47 +0300

    api key updated for personal proj

--- src/apis/lol.js
+++ src/apis/lol.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const KEY = 'RGAPI-bcca3a5c-cc96-42d9-b81e-9972c360a7c7';
+export const KEY = 'RGAPI-8e0d9dff-f73f-4f82-8fdf-0cb6133a36b8';
 
 export default axios.create({
   baseURL: 'https://cors-anywhere.herokuapp.com/https://euw1.api.riotgames.com/lol/summoner/v4/summoners/',
